6da500caeb4d1988008ad70eb60d775920a1a6db
Subhrajyoti <subha.ionic3@gmail.com>
2020-05-13 19:50:46 +0530

    Initial Commit

---
+++ WeSplitProject_1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WeSplitProject_1
+//
+//  Created by Subhrajyoti Chakraborty on 13/05/20.
+//  Copyright © 2020 Subhrajyoti Chakraborty. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
